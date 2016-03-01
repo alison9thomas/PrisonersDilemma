4015ca1cfe9740efc30b84826364f4fb2b30747e
alison9thomas <athomas17@middleburgacademy.org>
2016-03-01 09:54:37 -0500

    Update player3.py

--- player3.py
+++ player3.py
@@ -12,12 +12,12 @@ class Player3:
    #Defining functions for each child(instance) of class Car
    def strategy(self,history, opponent_history, score, opponent_score, getting_team_name):
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'betrayal'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
+            if len(opponent_history)==0: # betray is they were severaly punished last time 
+                return 'b'
             elif len(opponent_history)==1:
                 if history[-1]=='c' and opponent_history[-1]=='b':
                     return 'b' # betray is they were severely punished last time
